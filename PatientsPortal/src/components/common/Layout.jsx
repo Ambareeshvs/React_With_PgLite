b7faec3f6feabb868b91b003367279e6135c7f24
Ambareeshvs <ambareeshvs@gmail.com>
2025-05-23 18:06:43 +0530

    #commit with error correction

--- PatientsPortal/src/components/common/Layout.jsx
+++ PatientsPortal/src/components/common/Layout.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Outlet } from 'react-router-dom';
 import Navbar from "./Navbar";
-import Sidebar from "./SIdebar";
+import Sidebar from "./Sidebar";
 
 function Layout(){
     const [openSideBar, setSideBarOpen] = useState(false);
